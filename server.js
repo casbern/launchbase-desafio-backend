b871b4c5f70f472d1d947d5988f1b3eaf9066484
Cássia <cascrisbern@gmail.com>
2020-02-17 19:25:20 -0300

    addition to new routes

--- server.js
+++ server.js
@@ -11,13 +11,25 @@ server.use(express.static('public'))
 server.set("view engine", "njk") 
 
 nunjucks.configure("views", {
-  express: server
+  express: server,
+  autoescape: false,
+  noCache: true
 })
 
 //routes
 server.get("/", function(req,res) {
+  return res.render("courses")
+})
+
+server.get("/about", function(req, res) {
   return res.render("about")
 })
+
+//mostrar pagina 404 - página não encontrada
+server.use(function(req, res) {
+  res.status(404).render("not-found")
+})
+
 
 //server listening
 server.listen(5000, function() {
