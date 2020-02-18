b567837ae4f77bc219188a378bf53b2cf642a3e8
Cássia <cascrisbern@gmail.com>
2020-02-18 15:50:41 -0300

    bringing the backend data to the front

---
+++ data.js
@@ -0,0 +1,24 @@
+module.exports = [
+  {
+    id: "starter",
+    url: "https://skylab.rocketseat.com.br/static/64c237ccff807c054339a62d53b4b402.svg",
+    name: "Starter",
+    info: "3 módulos",
+    price: "Gratuito"
+  },
+  {
+    id: "launchbase",
+    url: "https://skylab.rocketseat.com.br/static/0828532024cb46921a6b5e941f8d788d.svg",
+    name: "Launchbase",
+    info: "6 módulos",
+    price: "Pago"
+  },
+  {
+    id: "gostack",
+    url: "https://skylab.rocketseat.com.br/static/83a178a0653dab1d55e2ed7946465975.svg",
+    name: "GoStack",
+    info: "10 módulos",
+    price: "Pago"
+  }
+
+]
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express") //chamando o express
 const nunjucks = require("nunjucks")
+const courses = require("./data")
 
 //server
 const server = express() //inicializando o express que vai fazer nosso servidor funcionar
@@ -12,17 +13,29 @@ server.set("view engine", "njk")
 
 nunjucks.configure("views", {
   express: server,
-  autoescape: false,
+  autoescape: false, //imprimir the html
   noCache: true
-})
+}) 
 
 //routes
 server.get("/", function(req,res) {
-  return res.render("courses")
+  return res.render("courses", { items: courses })
 })
 
 server.get("/about", function(req, res) {
-  return res.render("about")
+  const about = {
+    avatar_url: "https://pbs.twimg.com/profile_images/953595371875422210/0pWsfSSp_400x400.jpg",
+    name: "Rocketseat",
+    description: "Plataforma de educação em tecnologia",
+    tecnologies: [ "HTML", "CSS", "JavaScript"],
+    links: [
+      {name: "Github", url: "https://github.com/Rocketseat"},
+      {name: "Facebook", url: "https://www.instagram.com/rocketseat_oficial/?hl=en"},
+      {name: "Instagram", url: "https://www.facebook.com/rocketseat/"}
+    ]
+  }
+
+  return res.render("about", {about})
 })
 
 //mostrar pagina 404 - página não encontrada
@@ -35,4 +48,5 @@ server.use(function(req, res) {
 server.listen(5000, function() {
   console.log("server is running")
 })
+
 
